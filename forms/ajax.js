1596a2a5d591657e0c4c6bb6b6c9bd9c6e3fc8e4
Michael Chintu <michaelsinkamba211@gmail.com>
2024-03-29 17:52:22 +0200

    files added

---
+++ forms/ajax.js
@@ -0,0 +1,30 @@
+$(document).ready(function()){
+$("#submit").click(function()){
+var name = $("name").val();
+var email = $("email").val();
+var subject = $("subject").val();
+var message = $("message").val();
+var dataString = 'name='+name+ '$email='+email+ '$subject='+subject+ '$message='+message;
+if(name==''||email==''||subject==''||message=='') {
+$("#display").html("<div class = 'alert alert-warning'>Please fill all fields.</div>");
+
+}
+else if(dhjs)
+{
+$("#display").html("<div class = 'alert alert-warning'>Please fill all fields.</div>");
+
+}
+else{
+$.ajax({
+url:"contact.php",
+data: dataString,
+catche: false,
+success: function(result){
+$("#display").html(result);
+}
+});
+}
+
+return false;
+});
+});
---
+++ forms/contact.php
@@ -0,0 +1,39 @@
+<?php
+ 
+
+
+  
+  $receiving_email_address = 'kambelejoseph8@gmail.com';
+
+  if( file_exists($php_email_form = './assets/vendor/php-email-form/contact.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+  $contact->message = $_POST['message'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'kambelejoseph.online',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_name( $_POST['name'], 'From');
+  $contact->add_email( $_POST['email'], 'Email');
+  $contact->add_subject( $_POST['subject'], 'Subject');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
